6a018336503357a92d3d911e0214f0e72ab7630a
Daniel Castillo <danielcastillo89@icloud.com>
2023-03-30 02:21:12 -0500

    worked on module 4

--- index.js
+++ index.js
@@ -44,7 +44,22 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+    this.stomach = []
+  }
+  eat(edible) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(edible);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -62,7 +77,26 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, mpg) {
+    this.model = model;
+    this.milesPerGallon = mpg;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(dist) {
+    const driveableMiles = this.tank * this.milesPerGallon;
+    if (dist <= driveableMiles) {
+      this.odometer = this.odometer + dist;
+      this.tank = this.tank - (dist / this.milesPerGallon);
+    } else {
+      this.odometer = this.odometer + driveableMiles;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
 }
 
 /*
@@ -79,7 +113,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor(name, age, location) {
+    this.name = name;
+    this.age = age;
+    this.location = location
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -97,8 +138,19 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor({ name, age, location, specialty, favLanguage, catchPhrase }) {
+    super({ name, age, location, specialty, favLanguage, catchPhrase })
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`
+  }
+  grade(student, subject){
+return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
@@ -118,7 +170,7 @@ class Instructor {
 */
 
 class Student {
-   
+
 }
 
 /*
@@ -136,7 +188,7 @@ class Student {
 */
 
 class ProjectManager {
-   
+
 }
 
 /*
@@ -151,7 +203,7 @@ class ProjectManager {
 
 //End of Challenge
 /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
-function foo(){
+function foo() {
   return 'bar';
 }
 
